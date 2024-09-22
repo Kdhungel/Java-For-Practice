0a37821c6ba1eb98b2b5d7f76ef6dffd77b59d87
kdhungel <Kritishdhungel@gmail.com>
2024-09-22 16:48:50 +0545

    Test commit

--- Java/src/ControlStatement/forLoop.java
+++ Java/src/ControlStatement/forLoop.java
@@ -9,7 +9,7 @@ for(initialization; condition; inc/dec){
 public class forLoop {
     public static void main(String[] args) {
 //        int sum = 0;
-//        for(int i = 1; i <= 10; i++){
+//        for(int i = 1; i <= 10;  i++){
 //            sum = sum + i;
 //        }
 //        System.out.println("The sum of first 10 natural number is "+ sum);
